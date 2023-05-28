79f8c04163312b41c5dad71c56a11d70c07ea2cc
Degisew Mengist <jegisew21@gmail.com>
2023-05-28 13:05:01 +0300

    Registered customers

--- store/admin.py
+++ store/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
-from .models import Collection, Product
+from .models import Collection, Product, Customer
 
 # Customizing admin list page 
 @admin.register(Product) # shorter way of registering models
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'unit_price']
+    list_editable = ['unit_price']
+    list_per_page = 10
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership']
+    list_editable = ['membership']
+    list_per_page = 10
 
 
 admin.site.register(Collection)
-# admin.site.register(Product)
