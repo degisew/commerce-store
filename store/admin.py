1e174394ca2366c24c9bf87ce3185ec2cf017a46
Degisew Mengist <jegisew21@gmail.com>
2023-05-28 12:34:42 +0300

    Register models using decorator

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from.models import Collection, Product
+from .models import Collection, Product
+
+# Customizing admin list page 
+@admin.register(Product) # shorter way of registering models
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'unit_price']
 
 
 admin.site.register(Collection)
-admin.site.register(Product)
+# admin.site.register(Product)
