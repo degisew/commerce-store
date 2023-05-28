747bc64f12bc621549b1ec54b8ed7013d2802dd2
Degisew Mengist <jegisew21@gmail.com>
2023-05-28 13:08:20 +0300

    Implemented ordering logic

--- store/admin.py
+++ store/admin.py
@@ -7,12 +7,14 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'unit_price']
     list_editable = ['unit_price']
     list_per_page = 10
+    ordering = ['title', 'unit_price']
 
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'membership']
     list_editable = ['membership']
     list_per_page = 10
+    ordering = ['first_name', 'last_name']
 
 
 admin.site.register(Collection)
