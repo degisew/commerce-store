5c3f30ed5d4a2b2ab802d9ca3bfe8bf4f2f8e54c
Degisew Mengist <jegisew21@gmail.com>
2023-05-29 23:49:11 +0300

    Handeled Exception

--- store/views.py
+++ store/views.py
@@ -1,18 +1,20 @@
-import imp
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializers
+from store import serializers
 
 
 @api_view()
 def product_list(request):
-    return Response("Ok")
+    queryset = Product.objects.all()
+    serializer = ProductSerializers(queryset, many=True)
+    return Response(serializer.data)
 
 
 @api_view()
 def product_detail(request, id):
-    product = Product.objects.get(pk=id)
+    product = get_object_or_404(Product, pk=id)
     serializer = ProductSerializers(product)
     return Response(serializer.data)
