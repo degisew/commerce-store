0e0675939ca03d47d616a261a2bb8b3de45beebd
Degisew Mengist <jegisew21@gmail.com>
2023-05-29 23:39:31 +0300

    Overide  django COERCE_DEcCIMAL_TO_String setting

--- ecommerceStore/settings.py
+++ ecommerceStore/settings.py
@@ -131,3 +131,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Overriding django default behavior for decimal fields
+REST_FRAMEWORK = {
+    'COERCE_DECIMAL_TO_STRING': False
+}
