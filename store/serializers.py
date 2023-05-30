fdef6056b9bb3ca80ffcf2946da1bdd29664ab0a
Degisew <jegisew21@gmail.com>
2023-05-30 16:13:08 +0300

    Refactored to use ModelSerializer over Serializer

--- store/serializers.py
+++ store/serializers.py
@@ -2,9 +2,12 @@ from decimal import Decimal
 from rest_framework import serializers
 from .models import Product
 
-class ProductSerializers(serializers.Serializer):
-    title = serializers.CharField()
-    price = serializers.DecimalField(max_digits=5, decimal_places=2, source='unit_price')
+class ProductSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'unit_price', 'price_with_tax', 'collection']
+
+        
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
 
 
