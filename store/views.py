6f143ef2c5a6e45334f02cd7104f6b5693316fa0
Degisew Mengist <jegisew21@gmail.com>
2023-05-31 19:40:40 +0300

    Implemented CRUD

--- store/views.py
+++ store/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Product
+from django.db.models import Count
+from .models import OrderItem, Product
 from .serializers import ProductSerializers
 
 
@@ -19,7 +20,7 @@ def product_list(request):
         return Response(data=serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(['GET', 'PUT'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def product_detail(request, id):
     product = get_object_or_404(Product, pk=id)
     if request.method == 'GET':
@@ -30,3 +31,8 @@ def product_detail(request, id):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+    elif request.method == 'DELETE':
+        if product.orderitems.count() > 0:
+            return Response({"error": "The Object can't be deleted because it's associated with order items."}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
