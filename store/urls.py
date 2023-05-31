d012725d9afcfce02b13773ff8fc7f17b7230537
Degisew Mengist <jegisew21@gmail.com>
2023-05-31 23:25:42 +0300

    Added collection endpoint url

--- store/urls.py
+++ store/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import product_list, product_detail
+from .views import product_list, product_detail, collection_list, collection_detail
 
 
 urlpatterns = [
     path("products/", product_list, name="product_list"),
     path("products/<int:id>", product_detail, name="product_detail"),
+    path("collections/", collection_list, name="collection_list"),
+    path("collections/<int:id>", collection_detail, name="collection_detail"),
     ]
