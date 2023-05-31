61d063b8a2d0ac35361ad3872b1a34396695bf89
Degisew Mengist <jegisew21@gmail.com>
2023-05-31 19:41:28 +0300

    Rename default related name

--- store/models.py
+++ store/models.py
@@ -109,7 +109,7 @@ class Address(models.Model):
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
-    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name='orderitems')
     quantity = models.PositiveSmallIntegerField()
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
 
