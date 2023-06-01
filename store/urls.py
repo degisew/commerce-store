c71e1344f4a4f339cc92f2e0d79b58913552b13a
Degisew Mengist <jegisew21@gmail.com>
2023-06-01 22:40:19 +0300

    Implemented nested router

--- store/urls.py
+++ store/urls.py
@@ -1,12 +1,17 @@
+from cgitb import lookup
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import CollectionViewSet, ProductViewSet
+from rest_framework_nested import routers
+from .views import CollectionViewSet, ProductViewSet, ReviewViewSet
 
-router = DefaultRouter()
+router = routers.DefaultRouter()
 router.register('products', ProductViewSet)
 router.register('collections', CollectionViewSet)
 
-urlpatterns = router.urls
+procuct_routers = routers.NestedDefaultRouter(router, 'products', lookup = 'product')
+procuct_routers.register('reviews', ReviewViewSet, basename='product-reviews')
+
+urlpatterns = router.urls + procuct_routers.urls
 # [
 #     path("products/", ProductList.as_view(), name="ProductList"),
 #     path("products/<int:pk>", ProductDetail.as_view(), name="ProductDetail"),
