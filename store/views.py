07aae7921e789694f4588bb6d18c9e792bfde6e8
Degisew Mengist <jegisew21@gmail.com>
2023-06-02 22:38:58 +0300

    Implement filtering in queryset

--- store/views.py
+++ store/views.py
@@ -45,8 +45,11 @@ class CollectionViewSet(ModelViewSet):
 
 
 class ReviewViewSet(ModelViewSet):
-    queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+
+    def get_queryset(self):
+        queryset = Review.objects.filter(product_id=self.kwargs['product_pk'])
+        return queryset
 
     def get_serializer_context(self):
         return {'product_id': self.kwargs['product_pk']}
