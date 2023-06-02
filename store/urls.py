a202d4b2eae3b9eac1b09b765c3d7b3efb9a45b8
Degisew Mengist <jegisew21@gmail.com>
2023-06-02 23:50:17 +0300

    Remove unwanted code

--- store/urls.py
+++ store/urls.py
@@ -12,7 +12,6 @@ procuct_routers = routers.NestedDefaultRouter(router, 'products', lookup = 'prod
 procuct_routers.register('reviews', ReviewViewSet, basename='product-reviews')
 
 urlpatterns = router.urls + procuct_routers.urls
-# [
 #     path("products/", ProductList.as_view(), name="ProductList"),
 #     path("products/<int:pk>", ProductDetail.as_view(), name="ProductDetail"),
 #     path("collections/", CollectionList.as_view(), name="CollectionList"),
