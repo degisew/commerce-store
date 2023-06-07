5631f70d541d88fbb9c96f9eefe738db178580a5
Degisew Mengist <jegisew21@gmail.com>
2023-06-07 23:06:54 +0300

    Created a cart viewset

--- store/views.py
+++ store/views.py
@@ -1,11 +1,13 @@
+from pyexpat import model
 from django.db.models import Count
 from rest_framework import status
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from rest_framework.mixins import CreateModelMixin
 from rest_framework.response import Response
 from store.pagination import DefaultPagination
-from .models import Collection, OrderItem, Product, Review
-from .serializers import CollectionSerializer, ProductSerializer, ReviewSerializer
+from .models import Cart, Collection, OrderItem, Product, Review
+from .serializers import CartSerializer, CollectionSerializer, ProductSerializer, ReviewSerializer
 
 
 class ProductViewSet(ModelViewSet):
@@ -62,3 +64,8 @@ class ReviewViewSet(ModelViewSet):
 
     def get_serializer_context(self):
         return {'product_id': self.kwargs['product_pk']}
+    
+
+class CartiewSet(CreateModelMixin, GenericViewSet):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
