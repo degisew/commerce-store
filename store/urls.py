55ab26a53c02ff6ddcdb6efa593f4156f2290e37
Degisew Mengist <jegisew21@gmail.com>
2023-06-07 23:12:48 +0300

    Registered Carts URL

--- store/urls.py
+++ store/urls.py
@@ -2,11 +2,12 @@ from cgitb import lookup
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from rest_framework_nested import routers
-from .views import CollectionViewSet, ProductViewSet, ReviewViewSet
+from .views import CartiewSet, CollectionViewSet, ProductViewSet, ReviewViewSet
 
 router = routers.DefaultRouter()
 router.register('products', ProductViewSet, basename='product')
 router.register('collections', CollectionViewSet)
+router.register('carts', CartiewSet)
 
 procuct_routers = routers.NestedDefaultRouter(
     router, 'products', lookup='product')
