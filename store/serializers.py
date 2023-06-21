88a471e6588727fc95350d863cb44bad3dbca168
Degisew Mengist <jegisew21@gmail.com>
2023-06-21 17:35:43 +0300

    added a logic to Calculate total price

--- store/serializers.py
+++ store/serializers.py
@@ -33,16 +33,34 @@ class ReviewSerializer(serializers.ModelSerializer):
         product_id = self.context['product_id']
         return Review.objects.create(product_id=product_id, **validated_data)
 
+''' another product serializer b/c not want to return
+    all details of product inside cart items serializer '''
+class MiniProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'unit_price']
 
 class CartItemSerializer(serializers.ModelSerializer):
+    product = MiniProductSerializer()
+    total_price = serializers.SerializerMethodField()
+
+    def get_total_price(self, cart_item: CartItem):
+        return cart_item.product.unit_price * cart_item.quantity
+    
     class Meta:
         model = CartItem
-        fields = ['id', 'product', 'quantity']
+        fields = ['id', 'product', 'quantity', 'total_price']
+    
 
 
 class CartSerializer(serializers.ModelSerializer):
     id = serializers.UUIDField(read_only=True)
     items = CartItemSerializer(many=True)
+    total_price = serializers.SerializerMethodField()
+
+    def get_total_price(self, cart: Cart):
+       return sum([item.quantity * item.product.unit_price for item in cart.items.all()])
+    
     class Meta:
         model = Cart
-        fields = ['id', 'items'] 
+        fields = ['id', 'items', 'total_price'] 
