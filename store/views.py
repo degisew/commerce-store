1965225fe6388ae0ee5a83dfb217852d98d1ac00
Degisew Mengist <jegisew21@gmail.com>
2023-06-21 17:34:51 +0300

    Optimize query

--- store/views.py
+++ store/views.py
@@ -66,7 +66,7 @@ class ReviewViewSet(ModelViewSet):
     
 
 class CartViewSet(CreateModelMixin, RetrieveModelMixin, GenericViewSet):
-    queryset = Cart.objects.all()
+    queryset = Cart.objects.prefetch_related('items__product').all()
     serializer_class = CartSerializer
 
 class CartItemViewSet(ListModelMixin, GenericViewSet):
