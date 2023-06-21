5b811ae87cb6c3b881b992af589c9f37572c6483
Degisew Mengist <jegisew21@gmail.com>
2023-06-21 21:15:26 +0300

    Added Deleting Cart Logic

--- store/views.py
+++ store/views.py
@@ -2,7 +2,7 @@ from django.db.models import Count
 from rest_framework import status
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
-from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, ListModelMixin
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, ListModelMixin, DestroyModelMixin
 from rest_framework.response import Response
 from store.pagination import DefaultPagination
 from .models import Cart, CartItem, Collection, OrderItem, Product, Review
@@ -65,7 +65,7 @@ class ReviewViewSet(ModelViewSet):
         return {'product_id': self.kwargs['product_pk']}
     
 
-class CartViewSet(CreateModelMixin, RetrieveModelMixin, GenericViewSet):
+class CartViewSet(CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, GenericViewSet):
     queryset = Cart.objects.prefetch_related('items__product').all()
     serializer_class = CartSerializer
 
