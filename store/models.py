6c55aa39f93ad717e829e8ff7ca5ca046daab6ae
Degisew Mengist <jegisew21@gmail.com>
2023-06-25 21:37:35 +0300

    Registered Customer

--- store/models.py
+++ store/models.py
@@ -1,4 +1,5 @@
 from uuid import uuid4
+from django.conf import settings
 from django.db import models
 from django.core.validators import MinValueValidator
 
@@ -61,20 +62,22 @@ class Customer(models.Model):
         (SILVER_MEMBERSHIP, "Silver"),
     ]
 
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
     birth_date = models.DateTimeField(null=True)
     membership = models.CharField(
         max_length=1, choices=MEMBERSHIP_CHOICES, default=BRONZE_MEMBERSHIP
     )
-
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
-
+        return f"{self.user.first_name} {self.user.last_name}"
+    
+    def first_name(self):
+        return self.user.first_name
+    
+    def last_name(self):
+        return self.user.last_name
     class Meta:
-        ordering = ["first_name"]
+        ordering = ["user__first_name"]
 
 
 class Order(models.Model):
