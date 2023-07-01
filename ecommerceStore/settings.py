aec6d15c587547406487b826b004c7b3a6a97609
Degisew Mengist <jegisew21@gmail.com>
2023-07-01 16:31:06 +0300

    Configured djoser serializer

--- ecommerceStore/settings.py
+++ ecommerceStore/settings.py
@@ -142,8 +142,13 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-   'AUTH_HEADER_TYPES': ('JWT',),
+    'AUTH_HEADER_TYPES': ('JWT',),
 }
 
+DJOSER = {
+    'SERIALIZERS': {
+        'user_create': 'core.serializers.UserCreateSerializer',
+    }
+}
 
 AUTH_USER_MODEL = 'core.User'
