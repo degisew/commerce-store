df73eaed6a3fd6be5792848df5be3a936ad8522b
Degisew Mengist <jegisew21@gmail.com>
2023-07-02 23:02:19 +0300

    Registered djoser serializer

--- ecommerceStore/settings.py
+++ ecommerceStore/settings.py
@@ -148,6 +148,7 @@ SIMPLE_JWT = {
 DJOSER = {
     'SERIALIZERS': {
         'user_create': 'core.serializers.UserCreateSerializer',
+        'current_user': 'core.serializers.UserSerializer',
     }
 }
 
