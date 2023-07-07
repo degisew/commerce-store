fda21db1d056d59ed548f6fcfcd672b2981ca508
Degisew Mengist <jegisew21@gmail.com>
2023-07-07 16:45:17 +0300

    alter cart_id type

--- store/migrations/0001_initial.py
+++ store/migrations/0001_initial.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Cart',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.UUIDField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
             ],
         ),
