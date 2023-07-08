c03e37c79020fa0cd26cd9408cac7382fb390c26
Degisew Mengist <jegisew21@gmail.com>
2023-07-08 22:48:34 +0300

    Added validation

--- store/serializers.py
+++ store/serializers.py
@@ -136,6 +136,13 @@ class OrderSerializer(serializers.ModelSerializer):
 class CreateOrderSerializer(serializers.Serializer):
     cart_id = serializers.UUIDField()
 
+    def validate_cart_id(self, cart_id):
+        if not Cart.objects.filter(pk=cart_id).exists():
+            raise serializers.ValidationError('No cart with the given id was found')
+        if CartItem.objects.filter(cart_id=cart_id).count == 0:
+            raise serializers.ValidationError('No items found in the cart.')
+        return cart_id
+    
     def save(self, **kwargs):
         with transaction.atomic():
             cart_id = self.validated_data['cart_id']
