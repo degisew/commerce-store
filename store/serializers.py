d322c5f1cf92fea668343e7cc2a9b90af86e4c4e
Degisew Mengist <jegisew21@gmail.com>
2023-07-10 22:24:25 +0300

    Implemented cusyom signals

--- store/serializers.py
+++ store/serializers.py
@@ -1,8 +1,8 @@
 from decimal import Decimal
 from django.db import transaction
 from rest_framework import serializers
+from .signals import order_created
 from .models import Cart, CartItem, Collection, Customer, Order, OrderItem, Product, Review
-
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
@@ -171,5 +171,7 @@ class CreateOrderSerializer(serializers.Serializer):
             ]
             OrderItem.objects.bulk_create(order_items)
             Cart.objects.filter(pk=cart_id).delete()
+
+            order_created.send_robust(self.__class__, order=order)
 
             return order
