4792f92bf1fc867c72fd1a83a9c2a4e2536f38a5
Degisew Mengist <jegisew21@gmail.com>
2023-07-10 21:54:13 +0300

    Refactored

--- store/signals/handlers.py
+++ store/signals/handlers.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Customer
+from ..models import Customer
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_customer_for_new_user(sender, **kwargs):
